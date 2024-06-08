572bef4db6222d6c081105e444a5ff64eec74eab
Mounahbaieb <95724725+Mounahbaieb@users.noreply.github.com>
2024-06-08 15:47:41 +0100

    Update login.interceptor.ts

--- src/app/service/auth/login.interceptor.ts
+++ src/app/service/auth/login.interceptor.ts
@@ -13,7 +13,7 @@ export class LoginInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (request.url !== 'http://174.129.128.123:9001/login') {
+    if (request.url !== 'http://174.129.128.123:9001/api/login') {
     
       const token = localStorage.getItem('token');
       request = request.clone({
